e8593c91d0dfc665406988c44322ef44816ddfb8
Brandon Kim <bkim2490@gmail.com>
2019-01-16 18:17:49 +0900

    homepage wireframe complete

--- src/App.js
+++ src/App.js
@@ -2,36 +2,34 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
+import ButtonAppBar from './components/Navbar';
 
 class App extends Component {
-  state = {
-    response: '',
-  }
+  // state = {
+  //   response: '',
+  // }
 
-  componentDidMount() {
-    this.callApi()
-    .then(res => this.setState({ response: res.express }))
-    .catch(err => console.log(err));
-  }
+  // componentDidMount() {
+  //   this.callApi()
+  //   .then(res => this.setState({ response: res.express }))
+  //   .catch(err => console.log(err));
+  // }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+  // callApi = async () => {
+  //   const response = await fetch('/api/hello');
+  //   const body = await response.json();
 
-    if (response.status !== 200) throw Error(body.message);
+  //   if (response.status !== 200) throw Error(body.message);
 
-    return body;
-  }
+  //   return body;
+  // }
 
   render() {
     return (
       <div className="App">
         <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-          </nav>
-          <h1>Grocery List</h1>
-          <p>{this.state.response}</p>
+          <ButtonAppBar />
+          {/* <p>{this.state.response}</p> */}
         </header>
         <main>
           <Route exact path='/' component={Landing} />
