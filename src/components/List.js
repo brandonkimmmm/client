388af86fd832e2449a7a00d87155992eef07b653
Brandon Kim <bkim2490@gmail.com>
2019-01-21 00:18:56 +0900

    list show sets found list as state

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
+import ListTable from './ListTable';
 
 class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: {
+                name: '',
+                User: {
+                    username: ''
+                }
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.callApi()
+        .then(res => {
+            this.setState({
+                list: res.list
+            });
+        })
+        .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch(`/api/lists/${this.props.match.params.listId}`);
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    }
+
     render() {
         return (
             <section className="list">
-                {this.props.match.params.listId} List will go here;
+                <h1>{this.state.list.name}</h1>
+                <h4>Created By: {this.state.list.User.username}</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Jill</td>
+                            <td>Smith</td>
+                            <td>50</td>
+                        </tr>
+                        <tr>
+                            <td>Eve</td>
+                            <td>Jackson</td>
+                            <td>94</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
         )
     }
